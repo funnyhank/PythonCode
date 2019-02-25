0bbd4b56513d07326df18b6851db2fec68c2e72d
funnyhank <funnyhank@live.com>
2019-02-25 08:34:24 +0800

    add SROA

--- Python-master/scrapy_Cui.py
+++ Python-master/scrapy_Cui.py
@@ -1,19 +1,11 @@
 import requests
-from pyquery import PyQuery as pq
+from bs4 import BeautifulSoup
 
-
-url = 'https://www.zhihu.com/explore'
-header = {
-    'user-Agent':"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36"
-}
-html = requests.get(url, headers=header).text
-doc = pq(html)
-items = doc('.explore-tab .feed-item').items()
-for item in items:
-    question = item.find('h2').text()
-    author = item.find('.author-link-line').text()
-    answer = pq(item.find('.content').html()).text()
-    file = open('explore.txt', 'a', encoding='utf-8')
-    file.write('\n'.join([question,author,answer]))
-    file.write('\n'+'='*50+'\n')
-    file.close()
+response = requests.get('https://bbs.huaweicloud.com/blogs/2f31bf73ec9f11e8bd5a7ca23e93a891')
+#response.encoding = 'gbk'
+result = response.text
+soup = BeautifulSoup(result,'lxml')
+print(soup.title)
+print(soup.title.string)
+print(soup.head)
+print(soup.p)
