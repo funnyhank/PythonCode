292238e12cf1825c4c599b5342fcec7a9e8fce75
funnyhank <funnyhank@live.com>
2023-04-14 15:14:54 +0800

    处理每月账单

--- PdfReading.py
+++ PdfReading.py
@@ -9,5 +9,8 @@ import pdfplumber
 with pdfplumber.open("C:\\Users\\hank\\Downloads\\123.pdf") as pdf:
     for page in pdf.pages:
         text = page.extract_text()
-        txt_file = open("C:\\Users\\hank\\Downloads\\123.txt",mode='a',encoding='utf-8')
-        txt_file.write(text)
+        txt_file = open(
+            "C:\\Users\\hank\\Downloads\\123.txt",
+            mode='a',
+            encoding='utf-8')
+        print(text)
